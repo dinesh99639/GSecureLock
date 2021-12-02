b7623efc284ee4f8e16a17ed6d9098f93b94b95e
Dinesh Somaraju <dinesh99639@gmail.com>
2021-12-02 22:19:41 +0530

    Add functionality to go to dashboard on login

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,59 +1,103 @@
 import { useEffect, useState } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 
 import { getUserData } from '../api/drive';
 
 import { AppBar, Box, Toolbar, IconButton, Typography, Avatar } from '@mui/material';
 
 import MenuIcon from '@mui/icons-material/Menu';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 import './Header.css';
 
 const Header = (props) => {
-    let [user, setUser] = useState({
+    const history = useHistory();
+
+    const [path, setPath] = useState(history.location.pathname)
+    const [user, setUser] = useState({
         name: '',
         email: '',
         image: ''
     });
 
+    const handleHomeButtonClick = () => {
+        history.push("/");
+        setPath("/");
+    }
+
+    const handleLockButtonClick = async () => {
+        let redirectToDashboard = true;
+
+        if (!props.auth.isLoggedIn) {
+            await props.auth.login();
+            
+            if (props.encryptedData.length === 0) {
+                history.push("/setup_account");
+                redirectToDashboard = false;
+            }
+        }
+
+        if (redirectToDashboard) {
+            history.push("/dashboard");
+            setPath("/dashboard");
+        }
+    }
+
     useEffect(() => {
         const init = async () => {
-            let res = await getUserData();
-
-            setUser({
-                name: res.user.displayName,
-                email: res.user.emailAddress,
-                image: res.user.photoLink
-            });
+            if (props.auth.isLoggedIn) {
+                let res = await getUserData();
+                setUser({
+                    name: res.user.displayName,
+                    email: res.user.emailAddress,
+                    image: res.user.photoLink
+                });
+            }
         }
         init();
-    }, []);
+    }, [props.auth.isLoggedIn]);
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box>
             <AppBar position="static" className="main-header" >
-                <Toolbar style={{ minHeight: 36 }} >
+                <Toolbar style={{ minHeight: "5vh" }} >
                     <IconButton
                         size="large"
                         edge="start"
                         color="inherit"
                         aria-label="menu"
-                        sx={{ mr: 2, display: { md: "none" } }}
+                        sx={{ display: { md: "none" } }}
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} >GRISP</Typography>
 
-                    <Box style={{ display: "flex" }}>
-                        <IconButton size="large" onClick={props.toggleTheme} >
-                            {(props.theme === "light")?
-                                <DarkModeIcon style={{ margin: "0 10px" }} />:
-                                <LightModeIcon style={{ margin: "0 10px", color: "white" }} />
+                    <Box style={{ marginRight: "auto" }}>
+                        <Typography 
+                            variant="h6" 
+                            component="div" 
+                            sx={{ cursor: "pointer", width: "auto" }} 
+                            onClick={handleHomeButtonClick} 
+                        >GSecurePass</Typography>
+                    </Box>
+
+                    <Box style={{ display: "flex", alignItems: "center" }}>
+                        <Link to="/test" style={{ color: "white" }}>Test</Link>
+
+                        <IconButton size="medium" onClick={props.toggleTheme} >
+                            {(props.theme === "light") ?
+                                <DarkModeIcon style={{ color: "black" }} /> :
+                                <LightModeIcon style={{ color: "white" }} />
                             }
-                            
                         </IconButton>
-                        <Avatar alt={user.name} src={user.image} sx={{ width: 30, height: 30, margin: "auto" }} />
+
+                        {(path === "/") ?
+                            <IconButton size="medium" onClick={handleLockButtonClick}>
+                                <LockOpenIcon style={{ color: "white" }} />
+                            </IconButton> :
+                            <Avatar alt={user.name} src={user.image} sx={{ width: 25, height: 25, margin: "auto 5px" }} />
+                        }
                     </Box>
                 </Toolbar>
             </AppBar>
