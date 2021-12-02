eaeb8585ae4543aeb4d8a35b4835956f943053d8
Dinesh Somaraju <dinesh99639@gmail.com>
2021-12-02 22:18:45 +0530

    sample data

---
+++ src/sampleData.js
@@ -0,0 +1,20 @@
+const data = {
+    config: {
+        theme: "",
+        timer: "",
+    },
+    templates: [{
+        id: "",
+        name: "",
+        data: [
+            { name, value, hide }
+        ]
+    }],
+    credentials: [{
+        id: "",
+        name: "",
+        data: [
+            { user }
+        ]
+    }]
+}
