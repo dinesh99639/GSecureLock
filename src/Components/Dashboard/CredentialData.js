d9c941cf300c3e20933d4eac39eabb90783620a3
Dinesh Somaraju <dinesh99639@gmail.com>
2021-12-02 22:25:50 +0530

    Add credential data template

---
+++ src/Components/Dashboard/CredentialData.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+
+function CredentialData(props) {
+    return (<>
+        <Box className="borderRight" style={{ height: "100%", width: "100%" }} >
+            <Box>
+                
+            </Box>
+        </Box>
+    </>);
+}
+
+export default CredentialData;
