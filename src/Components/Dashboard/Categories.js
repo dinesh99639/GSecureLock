2e627c869000ce8edaf082b253c5655222a178a9
Dinesh Somaraju <dinesh99639@gmail.com>
2021-12-09 23:51:32 +0530

    Add Category column to dashboard

---
+++ src/Components/Dashboard/Categories.js
@@ -0,0 +1,34 @@
+import { Box, Typography } from "@mui/material";
+
+function Categories(props) {
+    const { categoriesCount, selectedCategory, updateSelectedCategory } = props;
+
+    return (<>
+        <Box className="borderRight" style={{ height: "100%" }} >
+            <Box className="borderBottom" style={{ textAlign: "center", padding: "8px 0" }} >Categories</Box>
+
+            <Box>
+                {categoriesCount.map((category) => {
+                    return <Box
+                        key={category.name}
+                        className="borderBottom"
+                        style={{
+                            margin: "0 2px",
+                            padding: "5px 10px",
+                            cursor: "pointer",
+                            borderRadius: "4px",
+                            backgroundColor: (selectedCategory === category.name) ? "rgb(0, 136, 253)" : null,
+                            color: (selectedCategory === category.name) ? "white" : "inherit",
+                        }}
+                        onClick={() => updateSelectedCategory(category.name)}
+                    >
+                        <Typography style={{ fontSize: "16px" }} >{category.name}</Typography>
+                        <Typography style={{ fontSize: "14px", opacity: 0.7 }} >Total: {category.count}</Typography>
+                    </Box>
+                })}
+            </Box>
+        </Box>
+    </>);
+}
+
+export default Categories;
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -5,16 +5,25 @@ import { useState, useEffect } from 'react';
 import { Grid } from '@mui/material';
 
 import Timebar from './Timebar';
+import Categories from './Categories';
 import CredentialsList from './CredentialsList';
 import CredentialData from './CredentialData';
 import LockScreen from './LockScreen';
 
 function Dashboard(props) {
     const isDesktop = window.innerWidth > 760;
-    const { setState } = props;
+    const { state, setState } = props;
 
     // const [lockTime, updateLockTime] = useState({ m: 5, s: 0, lockAt: new Date().getTime() + 300000 });
     const [lockTime, updateLockTime] = useState({ m: 0, s: 0, lockAt: 0 });
+
+    const [selectedCategory, updateSelectedCategory] = useState('All');
+    const [selectedEntryId, updateSelectedEntryId] = useState('');
+
+    const [categoriesCount, updateCategoriesCount] = useState([]);
+
+    const [entriesById, updateEntriesById] = useState({});
+    const [filteredEntries, updateFilteredEntries] = useState([]);
 
     useEffect(() => {
         if ((lockTime.m <= 0) && (lockTime.s <= 0)) {
@@ -22,18 +31,85 @@ function Dashboard(props) {
         }
     }, [lockTime, setState]);
 
+    useEffect(() => {
+        let data = state.data;
+        
+        if (data) {
+            let credentialsById = {};
+            let categoriesCountObj = {};
+            let categoriesCountArr = [];
+            let totalEntries = 0;
+            data.credentials.forEach((entry) => {
+                credentialsById[entry.id] = entry;
+
+                if (categoriesCountObj[entry.category]) categoriesCountObj[entry.category]++;
+                else categoriesCountObj[entry.category] = 1;
+
+                totalEntries++;
+            });
+
+            categoriesCountArr.push({ name: 'All', count: totalEntries });
+            ['Passwords', 'API Keys', 'Cards', 'Coupons'].forEach((category) => {
+                if (categoriesCountObj[category]) categoriesCountArr.push({ name: category, count: categoriesCountObj[category] });
+                else categoriesCountArr.push({ name: category, count: 0 });
+
+                delete categoriesCountObj[category];
+            })
+
+            for (let category in  categoriesCountObj) categoriesCountArr.push({ name: category, count: categoriesCountObj[category] });
+
+            updateCategoriesCount(categoriesCountArr)
+            updateEntriesById(credentialsById);
+        }
+        else updateEntriesById({});
+
+    }, [state.data]);
+
+
     return (<>
         {(isDesktop) ? <>
-            <Grid container style={{ display: "flex", flex: 1 }} >
+            {/* <Grid container style={{ display: "flex", flex: 1 }} >
                 <Grid item xs={0.55} ><Timebar
                     lockTime={lockTime}
                     updateLockTime={updateLockTime}
                 /></Grid>
                 <Grid item xs={3} >
-                    <CredentialsList state={props.state} />
+                    <CredentialsList 
+                        state={props.state} 
+                        filteredEntries={filteredEntries}
+                        updateFilteredEntries={updateFilteredEntries}
+                        selectedEntryId={selectedEntryId}
+                        updateSelectedEntryId={updateSelectedEntryId}
+                    />
                 </Grid>
-                <Grid item xs={5.5} ><CredentialData /></Grid>
+                <Grid item xs={5.5} ><CredentialData
+                    entriesById={entriesById}
+                /></Grid>
                 <Grid item xs={2.95} ></Grid>
+            </Grid> */}
+
+            <Grid container style={{ display: "flex", flex: 1 }} >
+                <Grid item xs={0.46} ><Timebar
+                    lockTime={lockTime}
+                    updateLockTime={updateLockTime}
+                /></Grid>
+                <Grid item xs={1.7} >
+                    <Categories
+                        categoriesCount={categoriesCount}
+                        selectedCategory={selectedCategory}
+                        updateSelectedCategory={updateSelectedCategory}
+                    />
+                </Grid>
+                <Grid item xs={2.5} >
+                    <CredentialsList
+                        state={props.state}
+                        selectedCategory={selectedCategory}
+                        filteredEntries={filteredEntries}
+                        updateFilteredEntries={updateFilteredEntries}
+                        selectedEntryId={selectedEntryId}
+                        updateSelectedEntryId={updateSelectedEntryId}
+                    />
+                </Grid>
             </Grid>
         </> : <>
             Mobile View
