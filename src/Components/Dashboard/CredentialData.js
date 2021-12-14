8550876c6b65847a9b8a162512fe3b0e725bfbc7
Dinesh Somaraju <dinesh99639@gmail.com>
2021-12-14 22:24:21 +0530

    Add credentialData page

--- src/Components/Dashboard/CredentialData.js
+++ src/Components/Dashboard/CredentialData.js
@@ -1,11 +1,157 @@
-import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+
+import { makeStyles } from "@mui/styles";
+import { Box, IconButton, TextField, Typography, Autocomplete, Paper, Grid } from "@mui/material";
+
+import SaveIcon from '@mui/icons-material/Save';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+
+const useInputStyles = makeStyles({
+    root: {
+        borderBottom: "1px solid white"
+    },
+    input: {
+        color: "inherit"
+    },
+    inputLabelNoShrink: {
+        transform: "translate(5px, -3px) scale(0.8)"
+    }
+});
+
+const autoCompleteStyles = makeStyles((theme) => ({
+    inputRoot: {
+        color: "inherit",
+        backgroundColor: "inherit",
+    },
+    clearIndicator: {
+        color: "inherit"
+    }
+}));
+
+function Input(props) {
+    const classes = useInputStyles();
+
+    return <TextField
+        variant="standard"
+        {...props}
+        className={classes.root}
+        InputLabelProps={{
+            shrink: false,
+            style: { color: 'inherit' },
+            className: classes.inputLabelNoShrink
+        }}
+    />
+}
+
+function SelectCategory({ name, categories, entryData, onChange }) {
+    const classes = autoCompleteStyles();
+
+    const handleChange = (e, type) => {
+        let value = ""
+        if (e.target.innerHTML[0] !== "<") {
+            value = (type === "list") ? e.target.innerHTML : e.target.value;
+        }
+
+        onChange({
+            target: {
+                name: "category",
+                value
+            }
+        });
+    }
+
+    return <Autocomplete
+        // disableClearable
+        options={Object.keys(categories)}
+        freeSolo={true}
+        value={entryData.category}
+        name="category"
+        classes={classes}
+        onChange={(e) => handleChange(e, "list")}
+        PaperComponent={({ children }) => (
+            <Paper style={{ background: "inherit", color: "inherit" }}>{children}</Paper>
+        )}
+        renderInput={(params) => <Input
+            name="category"
+            label="Category"
+            onChange={(e) => handleChange(e, "input")}
+            {...params}
+            inputProps={{ ...params.inputProps, style: { padding: "0 5px" } }}
+        />}
+    />
+}
 
 function CredentialData(props) {
+    const classes = useInputStyles();
+    const { selectedEntryId, categories } = props;
+
+    const [isEditMode, updateEditModeStatus] = useState(false);
+    const [entryData, updateEntryData] = useState(props.entryData);
+
+    const updateMetaInput = (e) => updateEntryData((state) => ({ ...state, [e.target.name]: e.target.value }))
+
+    useEffect(() => {
+        console.log(entryData);
+    }, [entryData])
+
+    useEffect(() => {
+        // console.log(props.entryData);
+        updateEditModeStatus(false);
+        updateEntryData(props.entryData)
+    }, [props.entryData, selectedEntryId])
+
     return (<>
         <Box className="borderRight" style={{ height: "100%", width: "100%" }} >
-            <Box>
-                
-            </Box>
+            {(isEditMode) ? <>
+                <Box className="borderBottom" style={{ padding: "5px 10px", display: "flex", justifyContent: "space-between" }} >
+                    <Input
+                        name="name"
+                        value={entryData.name}
+                        InputProps={{ className: classes.input }}
+                        inputProps={{ style: { padding: "0 5px" } }}
+                        onChange={updateMetaInput}
+                    />
+
+                    <Box>
+                        <IconButton size="small" style={{ color: "inherit", margin: "0 5px", padding: 0 }} ><SaveIcon /></IconButton>
+                        <IconButton size="small" style={{ color: "red", margin: "0 5px", padding: 0 }} ><DeleteOutlinedIcon /></IconButton>
+                    </Box>
+                </Box>
+
+                <Box className="borderBottom" style={{ padding: "10px 0" }} >
+                    <Grid container>
+                        <Grid item xs={6} style={{ padding: "0 5px" }} >
+                            <SelectCategory
+                                name="category"
+                                entryData={entryData}
+                                categories={categories}
+                                onChange={updateMetaInput}
+                            />
+                        </Grid>
+                        <Grid item xs={6} style={{ padding: "0 5px" }} >
+                            <Input
+                                label="User"
+                                name="user"
+                                value={entryData.user}
+                                InputProps={{ className: classes.input }}
+                                inputProps={{ style: { padding: "0 5px" } }}
+                                onChange={updateMetaInput}
+                            />
+                        </Grid>
+                    </Grid>
+
+                </Box>
+
+            </> : <>
+                <Box className="borderBottom" style={{ padding: "5px 10px", display: "flex", justifyContent: "space-between" }} >
+                    <Typography style={{ fontWeight: "bold" }} >{entryData.name}</Typography>
+                    <Box>
+                        <IconButton size="small" style={{ color: "#009dcd", margin: "0 5px", padding: 0 }} onClick={() => updateEditModeStatus(true)} ><EditOutlinedIcon /></IconButton>
+                        <IconButton size="small" style={{ color: "red", margin: "0 5px", padding: 0 }} ><DeleteOutlinedIcon /></IconButton>
+                    </Box>
+                </Box>
+            </>}
         </Box>
     </>);
 }
