a427a38fe9691e3f19ccc4eb3800af87c8a5e784
Dinesh Somaraju <dinesh99639@gmail.com>
2021-12-31 13:25:04 +0530

    Add Create Template to EntryInfo Component

--- src/Components/Dashboard/EntryOptions/EntryInfo.js
+++ src/Components/Dashboard/EntryOptions/EntryInfo.js
@@ -1,27 +1,49 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, Button } from "@mui/material";
 
 function EntryInfo(props) {
     return (<>
-        <Box className="borderBottom" style={{ textAlign: "center", padding: "8px 0" }} >Entry Info</Box>
         <Box
             style={{
-                padding: "10px"
+                height: "100%",
+                display: "flex",
+                flexDirection: "column"
             }}
         >
-            <Grid container style={{ padding: "5px 0" }} >
-                <Grid item xs={4}>Created:</Grid>
-                <Grid item xs={8}>
-                    <Typography style={{ fontSize: "12.5px" }} >{new Date().toString().substring(4, 15)}</Typography>
-                    <Typography style={{ fontSize: "12.5px" }} >{new Date().toString().substring(0, 3) + ", " + new Date().toString().substring(16, 24)}</Typography>
+            <Box className="borderBottom" style={{ textAlign: "center", padding: "8px 0" }} >Entry Info</Box>
+            <Box style={{ padding: "10px" }} >
+                <Grid container style={{ padding: "5px 0" }} >
+                    <Grid item xs={4}>Created:</Grid>
+                    <Grid item xs={8}>
+                        <Typography style={{ fontSize: "12.5px" }} >{new Date().toString().substring(4, 15)}</Typography>
+                        <Typography style={{ fontSize: "12.5px" }} >{new Date().toString().substring(0, 3) + ", " + new Date().toString().substring(16, 24)}</Typography>
+                    </Grid>
                 </Grid>
-            </Grid>
-            <Grid container style={{ padding: "5px 0" }} >
-                <Grid item xs={4}>Last Modified:</Grid>
-                <Grid item xs={8}>
-                    <Typography style={{ fontSize: "12.5px" }} >{new Date().toString().substring(4, 15)}</Typography>
-                    <Typography style={{ fontSize: "12.5px" }} >{new Date().toString().substring(0, 3) + ", 05:00 PM " + new Date().toString().substring(16, 24)}</Typography>
+                <Grid container style={{ padding: "5px 0" }} >
+                    <Grid item xs={4}>Last Modified:</Grid>
+                    <Grid item xs={8}>
+                        <Typography style={{ fontSize: "12.5px" }} >{new Date().toString().substring(4, 15)}</Typography>
+                        <Typography style={{ fontSize: "12.5px" }} >{new Date().toString().substring(0, 3) + ", 05:00 PM " + new Date().toString().substring(16, 24)}</Typography>
+                    </Grid>
                 </Grid>
-            </Grid>
+
+                <Box
+                    style={{
+                        height: "100%",
+                        display: "flex",
+                        flexFlow: "column",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}
+                >
+                    <Button
+                        style={{ 
+                            color: "inherit",
+                            textTransform: "none", 
+                            border: "1px solid gray"
+                        }}
+                    >Create Template</Button>
+                </Box>
+            </Box>
         </Box>
     </>);
 }
