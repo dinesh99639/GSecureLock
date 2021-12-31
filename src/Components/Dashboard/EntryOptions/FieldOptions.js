ebfe619a06db181978da6e781208bd64f57996fd
Dinesh Somaraju <dinesh99639@gmail.com>
2021-12-31 13:24:11 +0530

    Add Field Options layout

---
+++ src/Components/Dashboard/EntryOptions/FieldOptions.js
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+function FieldOptions(props) {
+    return (<>
+        <Box className="borderBottom" style={{ textAlign: "center", padding: "8px 0" }} >Field Options</Box>
+        <Box>
+            
+        </Box>
+    </>);
+}
+
+export default FieldOptions;
