bd15c43d5d169f258500adb0fdb28d6c63f58ad2
Dinesh Somaraju <dinesh99639@gmail.com>
2022-02-02 00:24:17 +0530

    UI design for account menu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,14 +2,17 @@ import { useEffect, useState, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory, Link } from 'react-router-dom';
 
+import { darkTheme } from '../Theme';
 import { getUserData } from '../api/drive';
 
-import { AppBar, Box, Toolbar, IconButton, Typography, Avatar } from '@mui/material';
+import { AppBar, Box, Toolbar, IconButton, Typography, Avatar, Menu, MenuItem, Divider } from '@mui/material';
 
 import MenuIcon from '@mui/icons-material/Menu';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Header = (props) => {
     const history = useHistory();
@@ -19,6 +22,9 @@ const Header = (props) => {
     const theme = useSelector((state) => state.config.theme);
     const setTheme = useCallback((theme) => dispatch({ type: "setTheme", payload: { theme } }), [dispatch]);
 
+    const [accountAnchorEl, setAccountAnchorEl] = useState(null);
+    const closeAccountMenu = () => setAccountAnchorEl(null);
+    const openAccountMenu = (event) => setAccountAnchorEl(event.currentTarget);
 
     const [path, setPath] = useState(history.location.pathname)
     const [user, setUser] = useState({
@@ -43,7 +49,7 @@ const Header = (props) => {
 
         if (!props.auth.isLoggedIn) {
             await props.auth.login();
-            
+
             if (localStorage.getItem('encryptedData').length === 0) {
                 history.push("/setup_account");
                 redirectToDashboard = false;
@@ -54,6 +60,10 @@ const Header = (props) => {
             history.push("/dashboard");
             setPath("/dashboard");
         }
+    }
+
+    const openGithubProject = () => {
+        window.open('https://github.com/dinesh99639/GSecurePass');
     }
 
     useEffect(() => {
@@ -72,17 +82,17 @@ const Header = (props) => {
                 }
                 else {
                     let res = await getUserData();
-    
+
                     userData = {
                         name: res.user.displayName,
                         email: res.user.emailAddress,
                         image: res.user.photoLink
                     }
-    
+
                     localStorage.setItem('userData', JSON.stringify(userData));
                 }
             }
-            
+
             setUser(userData);
         }
         loadUserData();
@@ -114,11 +124,11 @@ const Header = (props) => {
                     </IconButton>
 
                     <Box style={{ marginRight: "auto" }}>
-                        <Typography 
-                            variant="h6" 
-                            component="div" 
-                            sx={{ cursor: "pointer", width: "auto" }} 
-                            onClick={handleHomeButtonClick} 
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ cursor: "pointer", width: "auto" }}
+                            onClick={handleHomeButtonClick}
                         >GSecurePass</Typography>
                     </Box>
 
@@ -132,12 +142,81 @@ const Header = (props) => {
                             }
                         </IconButton>
 
-                        {(path === "/") ?
+                        {(path === "/") ? <>
                             <IconButton size="medium" onClick={handleLockButtonClick}>
                                 <LockOpenIcon style={{ color: "white" }} />
-                            </IconButton> :
-                            <Avatar alt={user.name} src={user.image} sx={{ width: 25, height: 25, margin: "auto 5px" }} />
-                        }
+                            </IconButton>
+                        </> : <>
+                            <IconButton
+                                aria-controls="menu-appbar"
+                                onClick={openAccountMenu}
+                            >
+                                <Avatar alt={user.name} src={user.image} sx={{ width: 25, height: 25, margin: "auto 5px" }} />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                sx={{
+                                    "& .MuiMenu-paper": {
+                                        width: "200px",
+                                        backgroundColor: (theme === "dark") ? darkTheme.backgroundColor : "white",
+                                        color: "inherit"
+                                    }
+                                }}
+                                anchorEl={accountAnchorEl}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(accountAnchorEl)}
+                                onClose={closeAccountMenu}
+                            >
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        alignItems: "center"
+                                    }}
+                                >
+                                    <Avatar alt={user.name} src={user.image} sx={{ width: 76, height: 76, margin: "auto 5px" }} />
+                                    <Typography sx={{ fontSize: "15px", margin: "5px 0 0 0", textAlign: "center" }} >{user.name}</Typography>
+                                    <Typography
+                                        sx={{
+                                            fontSize: "12px",
+                                            opacity: 0.76,
+                                            textAlign: "center"
+                                        }}
+                                    >@{user.email.split("@")[0]}</Typography>
+                                </Box>
+                                <Divider sx={{ backgroundColor: "inehrit", width: "100%", margin: "3px" }} />
+                                <MenuItem onClick={closeAccountMenu}>
+                                    <AccountCircleIcon fontSize='small' />
+                                    <Typography sx={{ padding: "0 0 0 7px" }} >Account</Typography>
+                                </MenuItem>
+                                <MenuItem onClick={closeAccountMenu}>
+                                    <LogoutIcon fontSize='small' />
+                                    <Typography sx={{ padding: "0 0 0 7px" }} >Sign out</Typography>
+                                </MenuItem>
+
+                                <Typography 
+                                    sx={{
+                                        margin: "10px 0 0 0",
+                                        fontSize: "13px",
+                                        textAlign: "center",
+                                        cursor: "pointer",
+
+                                        "&&:hover": {
+                                            textDecoration: "underline"
+                                        }
+                                    }}
+                                    onClick={openGithubProject} 
+                                >Github Project</Typography>
+                            </Menu>
+                        </>}
                     </Box>
                 </Toolbar>
             </AppBar>
