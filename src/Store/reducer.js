b6b58d5aa389fb57908fdb8164a7ad92e7186ca6
Dinesh Somaraju <dinesh99639@gmail.com>
2022-02-14 22:59:08 +0530

    add user variable to redux

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -4,7 +4,13 @@ const initState = {
         isLoading: false,
         isLoggedIn: null,
         snack: { open: false, type: 'success', message: "" },
-        entryOptionsMode: "EntryOptions"
+        entryOptionsMode: "EntryOptions",
+
+        user: {
+            name: '',
+            email: '',
+            image: ''
+        }
     },
 
     lockTime: { m: 0, s: 0, lockAt: 0 },
@@ -44,39 +50,40 @@ function reducer(state = initState, action) {
     switch (action.type) {
 
         // Config
-        case "setTheme":                        return { ...state, config: { ...state.config, theme: props.theme } }
-        case "updateLoadingStatus":             return { ...state, config: { ...state.config, isLoading: props.isLoading } }
-        case "updateSnack":                     return { ...state, config: { ...state.config, snack: props.snack } }
-        case "updateLoginStatus":               return { ...state, config: { ...state.config, isLoggedIn: props.isLoggedIn } }
-        case "updateEntryOptionsMode":          return { ...state, config: { ...state.config, entryOptionsMode: props.entryOptionsMode } }
-        
+        case "setTheme": return { ...state, config: { ...state.config, theme: props.theme } }
+        case "updateLoadingStatus": return { ...state, config: { ...state.config, isLoading: props.isLoading } }
+        case "updateSnack": return { ...state, config: { ...state.config, snack: props.snack } }
+        case "updateLoginStatus": return { ...state, config: { ...state.config, isLoggedIn: props.isLoggedIn } }
+        case "updateEntryOptionsMode": return { ...state, config: { ...state.config, entryOptionsMode: props.entryOptionsMode } }
+        case "setUser": return { ...state, config: { ...state.config, user: props.user } }
+
         // lockTime
-        case "updateLockTime":                  return { ...state, lockTime: { ...props.lockTime } }
-        
+        case "updateLockTime": return { ...state, lockTime: { ...props.lockTime } }
+
         // LocalStore
-        case "updateLocalStore":                return { ...state, localStore: { ...props.localStore } }
+        case "updateLocalStore": return { ...state, localStore: { ...props.localStore } }
 
         // Entries
-        case "updateEditModeStatus":            return { ...state, entries: { ...state.entries, isEditMode: props.isEditMode } }
-        case "updateIsTemplateMode":            return { ...state, entries: { ...state.entries, isTemplateMode: props.isTemplateMode } }
-        
-        case "updateSelectedCategory":          return { ...state, entries: { ...state.entries, selectedCategory: props.selectedCategory } }
-        case "updateCategories":                return { ...state, entries: { ...state.entries, categories: props.categories } }
-        case "updateCategoriesCount":           return { ...state, entries: { ...state.entries, categoriesCount: props.categoriesCount } }
-        case "updateEntriesById":               return { ...state, entries: { ...state.entries, entriesById: props.entriesById } }
-        case "updateSelectedEntryId":           return { ...state, entries: { ...state.entries, selectedEntryId: props.selectedEntryId } }
-        
-        case "updateSelectedEntryIndex":        return { ...state, entries: { ...state.entries, selectedEntryIndex: props.selectedEntryIndex } }
-        case "updateSelectedFieldIndex":        return { ...state, entries: { ...state.entries, selectedFieldIndex: props.selectedFieldIndex } }
-        
-        case "updateDrafts":                    return { ...state, entries: { ...state.entries, drafts: props.drafts } }
-        case "updateEntryData":                 return { ...state, entries: { ...state.entries, entryData: props.entryData } }
-        
-        case "updateSavedEntries":              return { ...state, entries: { ...state.entries, savedEntries: [...props.savedEntries] } }
-        case "updateModifiedEntries":           return { ...state, entries: { ...state.entries, modifiedEntries: [...props.modifiedEntries] } }
-        
-        case "updateTemplates":                 return { ...state, entries: { ...state.entries, templates: props.templates } }
-        
+        case "updateEditModeStatus": return { ...state, entries: { ...state.entries, isEditMode: props.isEditMode } }
+        case "updateIsTemplateMode": return { ...state, entries: { ...state.entries, isTemplateMode: props.isTemplateMode } }
+
+        case "updateSelectedCategory": return { ...state, entries: { ...state.entries, selectedCategory: props.selectedCategory } }
+        case "updateCategories": return { ...state, entries: { ...state.entries, categories: props.categories } }
+        case "updateCategoriesCount": return { ...state, entries: { ...state.entries, categoriesCount: props.categoriesCount } }
+        case "updateEntriesById": return { ...state, entries: { ...state.entries, entriesById: props.entriesById } }
+        case "updateSelectedEntryId": return { ...state, entries: { ...state.entries, selectedEntryId: props.selectedEntryId } }
+
+        case "updateSelectedEntryIndex": return { ...state, entries: { ...state.entries, selectedEntryIndex: props.selectedEntryIndex } }
+        case "updateSelectedFieldIndex": return { ...state, entries: { ...state.entries, selectedFieldIndex: props.selectedFieldIndex } }
+
+        case "updateDrafts": return { ...state, entries: { ...state.entries, drafts: props.drafts } }
+        case "updateEntryData": return { ...state, entries: { ...state.entries, entryData: props.entryData } }
+
+        case "updateSavedEntries": return { ...state, entries: { ...state.entries, savedEntries: [...props.savedEntries] } }
+        case "updateModifiedEntries": return { ...state, entries: { ...state.entries, modifiedEntries: [...props.modifiedEntries] } }
+
+        case "updateTemplates": return { ...state, entries: { ...state.entries, templates: props.templates } }
+
 
         default: return state
     }
