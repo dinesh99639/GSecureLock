2e83ecf5c95b7206cca75acae75f2b63caf49f3c
Dinesh Somaraju <dinesh99639@gmail.com>
2022-02-16 10:47:07 +0530

    Add elevation to import and export cards

--- src/Components/Account/importExport.js
+++ src/Components/Account/importExport.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@mui/styles";
 
-import { Box, Button, Grid, TextField, Typography } from "@mui/material";
+import { Box, Button, Grid, Paper, TextField, Typography } from "@mui/material";
 
 const useStyles = makeStyles({
     root: {
@@ -16,122 +16,130 @@ function ImportExport(props) {
     const classes = useStyles();
 
     return (<>
-        <Box
-            sx={{
-                margin: "0 10px"
-            }}
-        >
+        <Box>
             <Typography sx={{ fontSize: "18px", textAlign: "center", padding: "10px 0" }} >Import</Typography>
             <Grid container>
-                <Grid item className="borderRight" 
-                    xs={6}
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        backgroundColor: "rgba(0, 0, 0, 0.05)",
-                        padding: "10px 0"
-                    }}
-                >
-                    <Typography>Import a plain text file</Typography>
-                    <TextField
-                        variant="standard"
-                        placeholder="New password"
-                        className={classes.root}
-                        InputProps={{
-                            className: classes.input
-                        }}
-                        inputProps={{ style: { textAlign: 'center' } }}
-                        InputLabelProps={{
-                            style: { color: 'inherit' },
-                        }}
-                        style={{ margin: "10px 0" }}
-                    />
-                    <Button 
-                        variant="contained"
+                <Grid item xs={6}>
+                    <Paper
+                        elevation={2}
                         sx={{
-                            fontSize: "12px"
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            padding: "10px 0",
+                            margin: "0 10px",
+                            backgroundColor: "inherit",
+                            color: "inherit"
                         }}
-                    >Import</Button>
+                    >
+                        <Typography>Import a plain text file</Typography>
+                        <TextField
+                            variant="standard"
+                            placeholder="New password"
+                            className={classes.root}
+                            InputProps={{
+                                className: classes.input
+                            }}
+                            inputProps={{ style: { textAlign: 'center' } }}
+                            InputLabelProps={{
+                                style: { color: 'inherit' },
+                            }}
+                            style={{ margin: "10px 0" }}
+                        />
+                        <Button
+                            variant="contained"
+                            sx={{
+                                fontSize: "12px"
+                            }}
+                        >Import</Button>
+                    </Paper>
                 </Grid>
-                <Grid item
-                    xs={6}
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        backgroundColor: "rgba(0, 0, 0, 0.05)",
-                        padding: "10px 0"
-                    }}
-                >
-                    <Typography>Import an encrypted file</Typography>
-                    <Typography sx={{ padding: "16px", fontSize: "12px" }} >Password is same as the time of exporting</Typography>
-                    <Button 
-                        variant="contained"
+                <Grid item xs={6}>
+                    <Paper
+                        elevation={2}
                         sx={{
-                            fontSize: "12px"
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            padding: "10px 0",
+                            margin: "0 10px",
+                            backgroundColor: "inherit",
+                            color: "inherit"
                         }}
-                    >Import</Button>
+                    >
+                        <Typography>Import an encrypted file</Typography>
+                        <Typography sx={{ padding: "16px", fontSize: "12px" }} >Password is same as the time of exporting</Typography>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                fontSize: "12px"
+                            }}
+                        >Import</Button>
+                    </Paper>
                 </Grid>
             </Grid>
         </Box>
 
-        <Box
-            sx={{
-                margin: "0 10px"
-            }}
-        >
+        <Box>
             <Typography sx={{ fontSize: "18px", textAlign: "center", padding: "40px 0 10px 0" }} >Export</Typography>
             <Grid container>
-                <Grid item className="borderRight" 
-                    xs={6}
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        backgroundColor: "rgba(0, 0, 0, 0.05)",
-                        padding: "10px 0"
-                    }}
-                >
-                    <Typography>Export a plain text file</Typography>
-                    <TextField
-                        variant="standard"
-                        placeholder="Session password"
-                        className={classes.root}
-                        InputProps={{
-                            className: classes.input
-                        }}
-                        inputProps={{ style: { textAlign: 'center' } }}
-                        InputLabelProps={{
-                            style: { color: 'inherit' },
-                        }}
-                        style={{ margin: "10px 0" }}
-                    />
-                    <Button 
-                        variant="contained"
+                <Grid item xs={6} >
+                    <Paper
+                        elevation={2}
                         sx={{
-                            fontSize: "12px"
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            padding: "10px 0",
+                            margin: "0 10px",
+                            backgroundColor: "inherit",
+                            color: "inherit"
                         }}
-                    >Export</Button>
+                    >
+                        <Typography>Export a plain text file</Typography>
+                        <TextField
+                            variant="standard"
+                            placeholder="Session password"
+                            className={classes.root}
+                            InputProps={{
+                                className: classes.input
+                            }}
+                            inputProps={{ style: { textAlign: 'center' } }}
+                            InputLabelProps={{
+                                style: { color: 'inherit' },
+                            }}
+                            style={{ margin: "10px 0" }}
+                        />
+                        <Button
+                            variant="contained"
+                            sx={{
+                                fontSize: "12px"
+                            }}
+                        >Export</Button>
+                    </Paper>
                 </Grid>
-                <Grid item
-                    xs={6}
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        backgroundColor: "rgba(0, 0, 0, 0.05)",
-                        padding: "10px 0"
-                    }}
-                >
-                    <Typography>Export an encrypted file</Typography>
-                    <Typography sx={{ padding: "16px", fontSize: "12px" }} >Password is same as the session password</Typography>
-                    <Button 
-                        variant="contained"
+                <Grid item xs={6}>
+                    <Paper
+                        elevation={2}
                         sx={{
-                            fontSize: "12px"
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            padding: "10px 0",
+                            margin: "0 10px",
+                            backgroundColor: "inherit",
+                            color: "inherit"
                         }}
-                    >Export</Button>
+                    >
+                        <Typography>Export an encrypted file</Typography>
+                        <Typography sx={{ padding: "16px", fontSize: "12px" }} >Password is same as the session password</Typography>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                fontSize: "12px"
+                            }}
+                        >Export</Button>
+                    </Paper>
                 </Grid>
             </Grid>
         </Box>
