b2aa0f2ef99823327a82cbcdc094acfbf02d38aa
Dinesh Somaraju <dinesh99639@gmail.com>
2022-02-17 23:49:30 +0530

    Add Export functionality

--- src/Components/Account/importExport.js
+++ src/Components/Account/importExport.js
@@ -1,6 +1,10 @@
+import { useState, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from "@mui/styles";
 
 import { Box, Button, Grid, Paper, TextField, Typography } from "@mui/material";
+
+import crypto from '../../Utils/crypto';
 
 const useStyles = makeStyles({
     root: {
@@ -12,8 +16,70 @@ const useStyles = makeStyles({
     },
 });
 
-function ImportExport(props) {
+function ImportExport(props, fileType) {
+    const dispatch = useDispatch();
     const classes = useStyles();
+
+    const [sessionPassword, updateSessionPassword] = useState("");
+
+    const { encryptedData } = useSelector((state) => state.localStore);
+
+    const updateSnack = useCallback((snack) => dispatch({ type: "updateSnack", payload: { snack } }), [dispatch]);
+    const showSnack = (type, message) => updateSnack({ open: true, type, message, key: new Date().getTime() });
+
+    function getFileName() {
+        const fileName = "GSecureLock_";
+
+        const time = new Date();
+
+        let YYYY = time.getFullYear();
+
+        let MM = (time.getMonth() + 1).toString();
+        MM = (MM.length === 1) ? ("0" + MM) : MM;
+        
+        let DD = time.getDate().toString();
+        DD = (DD.length === 1) ? ("0" + DD) : DD;
+
+        let hh = time.getHours().toString();
+        hh = (hh.length === 1) ? ("0" + hh) : hh;
+
+        let mm = time.getMinutes().toString();
+        mm = (mm.length === 1) ? ("0" + mm) : mm;
+
+        let ss = time.getSeconds().toString();
+        ss = (ss.length === 1) ? ("0" + ss) : ss;
+
+        let timeFormat = YYYY + "-" + MM + "-" + DD + "T" + hh + "." + mm + "." + ss;
+
+        return fileName + timeFormat;
+    }
+
+    const saveToFile = (data, fileType) => {
+        const element = document.createElement("a");
+        const file = new Blob([data], { type: 'text/plain' });
+        element.href = URL.createObjectURL(file);
+
+        element.download = getFileName() + "." + fileType;
+        // document.body.appendChild(element);
+        element.click();
+    }
+
+    const getAppEntries = () => {
+        try {
+            return JSON.parse(crypto.decrypt(encryptedData, sessionPassword));
+        }
+        catch {
+            showSnack("error", "Wrong Session password");
+            return false;
+        }
+    }
+
+    const exportPlainTextFile = () => {
+        let entries = JSON.stringify(getAppEntries(), undefined, 2);
+        if (entries !== "false") saveToFile(entries, "txt");
+    }
+
+    const exportEncryptedFile = () => saveToFile(encryptedData, "gslock");
 
     return (<>
         <Box>
@@ -35,7 +101,7 @@ function ImportExport(props) {
                         <Typography>Import a plain text file</Typography>
                         <TextField
                             variant="standard"
-                            placeholder="New password"
+                            placeholder="Session password"
                             type="password"
                             className={classes.root}
                             InputProps={{
@@ -46,6 +112,8 @@ function ImportExport(props) {
                                 style: { color: 'inherit' },
                             }}
                             style={{ margin: "10px 0" }}
+                            value={sessionPassword}
+                            onChange={(e) => updateSessionPassword(e.target.value)}
                         />
                         <Button
                             variant="contained"
@@ -69,7 +137,22 @@ function ImportExport(props) {
                         }}
                     >
                         <Typography>Import an encrypted file</Typography>
-                        <Typography sx={{ padding: "16px", fontSize: "12px" }} >Password is same as the time of exporting</Typography>
+                        <TextField
+                            variant="standard"
+                            placeholder="Session password"
+                            type="password"
+                            className={classes.root}
+                            InputProps={{
+                                className: classes.input
+                            }}
+                            inputProps={{ style: { textAlign: 'center' } }}
+                            InputLabelProps={{
+                                style: { color: 'inherit' },
+                            }}
+                            style={{ margin: "10px 0" }}
+                            value={sessionPassword}
+                            onChange={(e) => updateSessionPassword(e.target.value)}
+                        />
                         <Button
                             variant="contained"
                             sx={{
@@ -111,12 +194,15 @@ function ImportExport(props) {
                                 style: { color: 'inherit' },
                             }}
                             style={{ margin: "10px 0" }}
+                            value={sessionPassword}
+                            onChange={(e) => updateSessionPassword(e.target.value)}
                         />
                         <Button
                             variant="contained"
                             sx={{
                                 fontSize: "12px"
                             }}
+                            onClick={exportPlainTextFile}
                         >Export</Button>
                     </Paper>
                 </Grid>
@@ -134,12 +220,13 @@ function ImportExport(props) {
                         }}
                     >
                         <Typography>Export as an encrypted file</Typography>
-                        <Typography sx={{ padding: "16px", fontSize: "12px" }} >Password is same as the session password</Typography>
+                        <Typography sx={{ padding: "17.5px", fontSize: "12px" }} >Password is same as the session password</Typography>
                         <Button
                             variant="contained"
                             sx={{
                                 fontSize: "12px"
                             }}
+                            onClick={exportEncryptedFile}
                         >Export</Button>
                     </Paper>
                 </Grid>
