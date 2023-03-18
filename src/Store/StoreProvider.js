972d0a9e88aa5254bb5d33de64039e1d15c10d0e
Dinesh Somaraju <dinesh99639@gmail.com>
2023-03-18 22:49:12 +0530

    Update packages and add redux dev tools

--- src/Store/StoreProvider.js
+++ src/Store/StoreProvider.js
@@ -1,9 +1,10 @@
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from './reducer';
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 function StoreProvider(props) {
     return <Provider store={store} >{props.children}</Provider>
